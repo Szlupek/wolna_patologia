0a632209686e815b492795eecd347f447d21534b
Michał Artur Szlupowicz <m.szlupowicz@gmail.com>
2024-12-05 23:04:13 +0100

    make appka more edgy

--- pato_appka/app.py
+++ pato_appka/app.py
@@ -1,17 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
+import random
 
 app = Flask(__name__)
 
+# Simple list of edgy quotes
+quotes = [
+    "Life is hard, but it's harder when you're stupid.",
+    "You only live once, but if you do it right, once is enough.",
+    "Everything is temporary, including your problems.",
+    "Success is not the key to happiness. Happiness is the key to success.",
+    "If you want to go fast, go alone. If you want to go far, go together."
+]
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/about')
-def about():
-    return "This is the About page!"
+@app.route('/quote')
+def quote():
+    return jsonify({'quote': random.choice(quotes)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
