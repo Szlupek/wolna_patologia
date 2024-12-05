a3db89c47e1e4bc80ebdb09bd0774d226d1fe0bc
Michał Artur Szlupowicz <m.szlupowicz@gmail.com>
2024-12-05 23:46:49 +0100

    blacked

--- pato_appka/app.py
+++ pato_appka/app.py
@@ -9,16 +9,19 @@ quotes = [
     "You only live once, but if you do it right, once is enough.",
     "Everything is temporary, including your problems.",
     "Success is not the key to happiness. Happiness is the key to success.",
-    "If you want to go fast, go alone. If you want to go far, go together."
+    "If you want to go fast, go alone. If you want to go far, go together.",
 ]
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/quote')
+
+@app.route("/quote")
 def quote():
-    return jsonify({'quote': random.choice(quotes)})
+    return jsonify({"quote": random.choice(quotes)})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
