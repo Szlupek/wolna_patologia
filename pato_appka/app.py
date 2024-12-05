4869370f466c3e34271162728704165a7d100c99
Michał Artur Szlupowicz <m.szlupowicz@gmail.com>
2024-12-05 22:27:30 +0100

    update home

--- pato_appka/app.py
+++ pato_appka/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return "Witaj na woli"
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
