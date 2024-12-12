31d672652606e5d11a4f6d13954037a764f53018
michal-szlupowicz-allegro <michal.szlupowicz@allegro.com>
2024-12-12 12:00:37 +0100

    Update app.py

--- pato_appka/app.py
+++ pato_appka/app.py
@@ -1,3 +1,5 @@
+"""Main file of our basic app."""
+
 from flask import Flask, render_template, jsonify
 import random
 
@@ -15,11 +17,30 @@ quotes = [
 
 @app.route("/")
 def home():
+    """
+    Function to handle the home route.
+
+    This function is responsible for handling requests to the root URL ("/") 
+    of the application. When a user navigates to this URL, this function 
+    will be called, and it will render the "index.html" template.
+
+    Returns:
+        A rendered HTML template for the home page.    """
     return render_template("index.html")
 
 
 @app.route("/quote")
 def quote():
+    """
+    Function to handle the quote route.
+    
+    This function is responsible for handling requests to the "/quote" URL
+    of the application. When a user navigates to this URL, this function
+    will be called, and it will return a random quote as a JSON response.
+
+    Returns:
+        A JSON object containing a random quote.
+    """
     return jsonify({"quote": random.choice(quotes)})
 
 
